eec2bfaf1edbf3b6b06c5cb8112a9729439dd4e0
tamas.molnar <liferay.appsec@liferay.com>
2021-10-27 13:11:52 +0200

    @tomtom-international/web-sdk-maps

--- tomtom-app/src/App.js
+++ tomtom-app/src/App.js
@@ -1,6 +1,18 @@
 import './App.css'
+let APP_KEY = "3ovwP0g2CzgNMyc7QtYqn6JuOHtaESTC";
 
 const App = () => {
+
+
+  useEffect(
+    () => {
+      map = new tt.map({
+        "key": APP_KEY,
+        "container":"mapElement",
+    });
+    }
+  );
+
   return (
     <div className="App">
     </div>
