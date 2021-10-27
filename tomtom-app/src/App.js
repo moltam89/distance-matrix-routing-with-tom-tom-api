0ec28b471eb41f7ddfc30d0a97f9b755ff208831
tamas.molnar <liferay.appsec@liferay.com>
2021-10-27 14:05:19 +0200

    Langtitude longtitude

--- tomtom-app/src/App.js
+++ tomtom-app/src/App.js
@@ -7,23 +7,38 @@ let APP_KEY = "3ovwP0g2CzgNMyc7QtYqn6JuOHtaESTC";
 
 const App = () => {
   const [map, setMap] = useState({});
+  const [lat, setLat] = useState("37.336634");
+  const [lng, setLng] = useState("-121.891623");
+
   const mapElement = useRef();
 
+  const goThere = (e) => {
+      if (e.target.dataset.id === "latitude") {
+        setLat(e.target.value);
+      }
+      else {
+        setLng(e.target.value);
+      }
+  }
+
   useEffect(() => {
-        let map = tt.map({
-              key: APP_KEY,
-              container: mapElement.current,
-              stylesVisibility: {
-                trafficIncidents: true,
-                trafficFlow: true,
-              },
-              zoom: 14
-            });
+      let map = tt.map({
+            key: APP_KEY,
+            container: mapElement.current,
+            stylesVisibility: {
+              trafficIncidents: true,
+              trafficFlow: true,
+            },
+            zoom: 5,
+            center: {lat, lng}, 
+          });
       setMap(map);
-    }, [] );
+    }, [lat, lng] );
 
   return (
     <div className="App">
+      <input id="lat" placeholder="latitude" type="text" data-id="latitude" onChange={goThere} />
+      <input id="lng" placeholder="longitude" type="text" data-id="longitude" onChange={goThere} />
       <div ref={mapElement} className="map">
       </div>
     </div>
