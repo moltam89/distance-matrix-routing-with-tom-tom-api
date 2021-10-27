3ed7c5398f365d060cdbe78d569d9bd25e784fb1
tamas.molnar <liferay.appsec@liferay.com>
2021-10-27 13:31:51 +0200

    Map

--- tomtom-app/src/App.js
+++ tomtom-app/src/App.js
@@ -1,20 +1,31 @@
 import './App.css'
+import * as tt from '@tomtom-international/web-sdk-maps';
+import {useEffect, useState, useRef} from "react";
+import '@tomtom-international/web-sdk-maps/dist/maps.css'
+
 let APP_KEY = "3ovwP0g2CzgNMyc7QtYqn6JuOHtaESTC";
 
 const App = () => {
+  const [map, setMap] = useState({});
+  const mapElement = useRef();
 
-
-  useEffect(
-    () => {
-      map = new tt.map({
-        "key": APP_KEY,
-        "container":"mapElement",
-    });
-    }
-  );
+  useEffect(() => {
+        let map = tt.map({
+              key: APP_KEY,
+              container: mapElement.current,
+              stylesVisibility: {
+                trafficIncidents: true,
+                trafficFlow: true,
+              },
+              zoom: 14
+            });
+      setMap(map);
+    }, [] );
 
   return (
     <div className="App">
+      <div ref={mapElement} className="map">
+      </div>
     </div>
   );
 }
