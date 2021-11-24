934af855d9ff8b228163ee8edf134d2578ede042
tamas.molnar <tamas.molnar@liferay.com>
2021-11-24 10:45:22 +0100

    Go to london link

--- tomtom-app/src/App.jsx
+++ tomtom-app/src/App.jsx
@@ -6,20 +6,28 @@ import '@tomtom-international/web-sdk-maps/dist/maps.css'
 
 let APP_KEY = "3ovwP0g2CzgNMyc7QtYqn6JuOHtaESTC";
 
+const BUDAPEST_LAT = "47.4979";
+const BUDAPEST_LNG = "19.0402";
+
+const LONDON_LAT = "51.5072";
+const LONDON_LNG = "0.1276";
+
 const App = () => {
   const [map, setMap] = useState({});
   const [zoom, setZoom] = useState(8);
 
-  const [lat, setLat] = useState("47.4979");
-  const [lng, setLng] = useState("19.0402");
+  const [lat, setLat] = useState(BUDAPEST_LAT);
+  const [lng, setLng] = useState(BUDAPEST_LNG);
 
   const mapElement = useRef();
   const latElement = useRef();
   const lngElement = useRef();
 
-  const goThere = () => {
-      let currentLat = latElement.current.value
-      let currentLng = lngElement.current.value
+  const goThere = (currentLat, currentLng) => {
+      if (!currentLat && !currentLng) {
+        currentLat = latElement.current.value
+        currentLng = lngElement.current.value
+      }
 
       if (currentLat && currentLng) {
         setLat(currentLat);
@@ -47,7 +55,10 @@ const App = () => {
 
       const addMarker = () => {
         const popupOffsets = {bottom: [0, -50]};
-        const popup = new tt.Popup({offset: popupOffsets}).setHTML("<b> Here I am");
+
+        const popupHTML = '<a id=anchorId href="#" >Go to London!</a>';
+
+        const popup = new tt.Popup({offset: popupOffsets}).setHTML(popupHTML);
 
         const element = document.createElement('div')
         element.className = 'marker'
@@ -66,10 +77,15 @@ const App = () => {
           setZoom(map.getZoom());
         })
 
-        marker.setPopup(popup);
+        marker.setPopup(popup).togglePopup();
 
       }
       addMarker();
+
+      document.getElementById("anchorId").addEventListener('click', event => {
+          goThere(LONDON_LAT, LONDON_LNG);
+        }, false
+      );
 
       return () => {
         map.remove()
